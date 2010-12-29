28ebea65c527bb7038cb49e355556a85eb5d0893
Julian Coutu <jcoutu@Julian-Coutus-MacBook-Pro>
2010-12-28 20:56:09 -0500

    this will set branches commits to a branch. rake

--- lib/gitnetworkitis/commit.rb
+++ lib/gitnetworkitis/commit.rb
@@ -1,8 +1,8 @@
 module GitNetworkitis
   class Commit < Base
     base_uri 'https://github.com/'
-    
-    attr_accessor :parents, :author, :time, :id, :date, :gravatar, :space, :message, :login, :branch
-    
+
+    attr_accessor :parents, :author, :time, :id, :date, :gravatar, :space, :message, :login, :network_meta
+
   end
 end
